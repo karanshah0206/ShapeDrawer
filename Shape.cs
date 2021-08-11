edd6c424dc391b37e425bb07440bf1a747c08079
Karan Shah <karanshah0206@outlook.com>
2021-08-11 15:14:38 +0530

    Basic Shape Classes Added To Draw Rectangles

---
+++ Shape.cs
@@ -0,0 +1,55 @@
+﻿using SplashKitSDK;
+
+namespace ShapeDrawer
+{
+    class Shape
+    {
+        private int _width, _height;
+        private float _x, _y;
+        private Color _color;
+
+        public Shape()
+        {
+            _x = 0;
+            _y = 0;
+            _width = 100;
+            _height = 100;
+            _color = Color.Green;
+        }
+
+        public Color Color
+        {
+            get { return _color; }
+            set { _color = value; }
+        }
+
+        public float X
+        {
+            get { return _x; }
+            set { _x = value; }
+        }
+
+        public float Y
+        {
+            get { return _y; }
+            set { _y = value; }
+        }
+
+        public int Width
+        {
+            get { return _width; }
+            set { _width = value; }
+        }
+
+        public int Height
+        {
+            get { return _height; }
+            set { _height = value; }
+        }
+
+        public void Draw()
+        {
+            SplashKit.FillRectangle(_color, _x, _y, _width, _height);
+        }
+    }
+}
