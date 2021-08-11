1121d0daac48901f498e665616b9aee95c4370b5
Karan Shah <karanshah0206@outlook.com>
2021-08-11 15:00:49 +0530

    Window Walking Skeleton

--- Program.cs
+++ Program.cs
@@ -4,6 +4,13 @@ public class Program
 {
     public static void Main()
     {
-        
+        new Window("Shape Drawer", 800, 600);
+        do
+        {
+            SplashKit.ProcessEvents();
+            SplashKit.ClearScreen();
+
+            SplashKit.RefreshScreen();
+        } while (!SplashKit.WindowCloseRequested("Shape Drawer"));
     }
 }
