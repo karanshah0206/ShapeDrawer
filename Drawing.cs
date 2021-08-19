1b036abbeef1dad92411450d13984be248ebf79a
Karan Shah <karanshah0206@outlook.com>
2021-08-19 10:45:58 +0530

    Created Framework for Drawing Class

---
+++ Drawing.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SplashKitSDK;
+
+namespace ShapeDrawer
+{
+    class Drawing
+    {
+        private List<Shape> _shapes = new List<Shape>();
+        private Color _background;
+
+        public Drawing()
+        { }
+
+        public List<Shape> SelectedShapes
+        { /* Add get handle here */ }
+
+        public int ShapeCount
+        { /* Add get handle here */ }
+
+        public Color Background
+        { /* Add get, set handles here */ }
+
+        public void Draw()
+        { }
+
+        public void SelectShapesAt(Point2D pt)
+        { }
+
+        public void AddShape(Shape s)
+        { }
+
+        public void RemoveShape(Shape s)
+        { }
+    }
+}
