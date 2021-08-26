ba63e5b3d705612489f23f9b90379a3c6a4f0532
Karan Shah <karanshah0206@outlook.com>
2021-08-26 10:55:47 +0530

    Line Selection and Deletion Implemented

--- Line.cs
+++ Line.cs
@@ -18,16 +18,27 @@ namespace ShapeDrawer
 
         public override void Draw()
         {
+            if (_selected) DrawOutline();
             SplashKit.DrawLine(_color, SplashKit.LineFrom(_x, _y, _x + _length, _y + _depth));
         }
 
         public override void DrawOutline()
         {
+            Circle c1 = new Circle(_color, 7);
+            Circle c2 = new Circle(_color, 7);
+
+            c1.X = _x; c1.Y = _y;
+            c2.X = _x + _length; c2.Y = _y + _length;
+
+            c1.Draw();
+            c2.Draw();
         }
 
         public override bool IsAt(Point2D pt)
         {
-            return false;
+            float lineSlope = _depth/_length;
+            float ptSlope = (float)((pt.Y - _y)/(pt.X - _x));
+            return ((lineSlope == ptSlope) && (pt.X >= _x) && (pt.X <= _x + _length) && (pt.Y >= _y) && (pt.Y <= _y + _depth));
         }
     }
 }
